aa721b317614afb7984d43b606a3313c507c2d22
胡重威 <ken@huchongweideMac-mini.local>
2021-03-11 21:58:49 +0800

    finish java and python version

--- c/data_structure/ch_2_5.java
+++ c/data_structure/ch_2_5.java
@@ -6,12 +6,12 @@ import java.util.Enumeration;
 程式目標見C語言版本註解.
 */
 class ch_2_5 {
-  private Hashtable<String, Boolean> rooms = new Hashtable<>();
+  private Hashtable<Integer, Boolean> rooms = new Hashtable<>();
 
   private void init() {
     for (int i=0; i<3; i++) {
       for (int j=0; j<3; j++) {
-        rooms.put("" + ((i + 1) * 100 + (j % 3 + 1)), false);
+        rooms.put((i + 1) * 100 + (j % 3 + 1), false);
       }
     }
   }
@@ -25,13 +25,21 @@ class ch_2_5 {
     System.out.println("input paid room number: (input 0 when finish)");
     while((roomNumber = scanner.nextInt()) != 0) {
       if (t.rooms.get(roomNumber) != null) {
-        t.rooms.put("" + roomNumber, true);
+        t.rooms.put(roomNumber, true);
       }
     }
 
-    Enumeration e = t.rooms.keys();
+    int totalPaid = 0;
+    int totalUnpaid = 0;
+    Enumeration<Boolean> e = t.rooms.elements();
     while(e.hasMoreElements()) {
-      System.out.println(e.nextElement());
+      if (e.nextElement()) {
+        totalPaid += 5000;
+      } else {
+        totalUnpaid += 5000;
+      }
     }
+
+    System.out.println(String.format("Total paid money= %d, unpaid money= %d", totalPaid, totalUnpaid));
   }
 }
