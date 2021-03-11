ec0e69a0e86cc45cc9da258a634c1a6be57a13ed
胡重威 <ken@huchongweideMac-mini.local>
2021-03-11 22:04:53 +0800

    add python try-catch

--- c/data_structure/ch_2_5.py
+++ c/data_structure/ch_2_5.py
@@ -1,4 +1,6 @@
-# 2 dimension list
+#
+# 程式目標見C語言版本註解.
+#
 rooms = [[False for j in range(3)] for i in range(3)]
 
 room_number = -1
@@ -6,7 +8,10 @@ while(room_number != 0):
     room_number = int(input('input room number: '))
     i = int(room_number / 100)
     j = int(room_number - i * 100)
-    rooms[i-1][j-1] = True
+    try:
+      rooms[i-1][j-1] = True
+    except IndexError:
+       print('wrong room number')  
 
 paid, unpaid = 0, 0
 for i in range(3):
@@ -15,5 +20,6 @@ for i in range(3):
             paid += 5000
         else:
             unpaid += 5000
+
 
 print(f'paid= {paid}, unpaid= {unpaid}')
