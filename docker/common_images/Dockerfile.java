02a68905b7d54129f00927ef2a7f5426d659cb4e
Ken Hu <kenhu1970@yahoo.com.tw>
2021-07-03 13:29:10 +0800

    use adoptopenjdk/openjdk11

--- docker/common_images/Dockerfile.java
+++ docker/common_images/Dockerfile.java
@@ -1,5 +1,7 @@
-FROM alpine:latest
-RUN apk --update --no-cache add openjdk16-jre
+# FROM adoptopenjdk/openjdk16:alpine-jre
+FROM adoptopenjdk/openjdk11:alpine-jre
+# FROM alpine:latest
+# RUN apk --update --no-cache add openjdk11-jre
 RUN apk --no-cache add bash
 RUN apk --no-cache add tzdata && \
         cp /usr/share/zoneinfo/Asia/Taipei /etc/localtime && \
