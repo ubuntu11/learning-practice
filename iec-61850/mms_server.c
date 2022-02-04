4f326990be527677d4746e0acc6c0ef26e433541
Ken Hu <kenhu1970@yahoo.com.tw>
2022-02-04 12:06:58 +0000

    add report and client code

--- iec-61850/mms_server.c
+++ iec-61850/mms_server.c
@@ -1,17 +1,38 @@
 /*
- * mms_server.c
+ * mms_server.c 參考libiec61850/examples/server_example_basic_io/server_example_basic_io.c.
  *
  *  Created on: 2022年2月3日
  *      Author: ken
  */
 
+#include "iec61850_server.h"
+#include "hal_thread.h"
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
 #include <math.h>
+#include "static_model.h"
 
-#define PI 3.1415926
+#define  PI 3.1415926
+#define  MMS_PORT 102
+
+// import IEC 61850 device model created from SCL-File
+extern   IedModel iedModel;
+
+static   int running = 0;
+static   IedServer iedServer = NULL;
+static void connectionHandler (IedServer self, ClientConnection connection, bool connected, void* parameter) {
+    if (connected)
+        printf("Connection opened\n");
+    else
+        printf("Connection closed\n");
+}
+
+void sigint_handler(int signalId) {
+    running = 0;
+}
 
 // 報價代碼聚合之所有交易資源加總之瞬時輸出/入總實功率
 struct GroupRealPowerStruc {
@@ -32,44 +53,135 @@ struct GroupInputStruc {
 	unsigned int quotationId;
 	// 輔助服務商品
 	unsigned char serviceCode;
-	// xxx
+	// 所有交易資源加總之瞬時輸出/入總實功率
 	GroupRealPower realPower[10];
 	// 報價代碼聚合之所有交易資源加總瞬時累計發電量
 	unsigned long supplyWatt;
 	// 報價代碼聚合之所有交易資源加總瞬時累計用電量
 	unsigned long demandWatt;
 	// 執行率計算時間點
-	unsigned long timestamp;
+	uint64_t timestamp;
 	// 執行率
 	unsigned int executionRate;
 };
 typedef struct GroupInputStruc GroupInput;
 
-// function prototypes
-void simulate();
+DataAttribute *groupRealPowerTsHighDaNames[10] = {IEDMODEL_Device1_GROGGIO01_AnIn1_mag_i, IEDMODEL_Device1_GROGGIO02_AnIn1_mag_i, IEDMODEL_Device1_GROGGIO03_AnIn1_mag_i,
+		IEDMODEL_Device1_GROGGIO04_AnIn1_mag_i, IEDMODEL_Device1_GROGGIO05_AnIn1_mag_i, IEDMODEL_Device1_GROGGIO06_AnIn1_mag_i,
+		IEDMODEL_Device1_GROGGIO07_AnIn1_mag_i, IEDMODEL_Device1_GROGGIO08_AnIn1_mag_i, IEDMODEL_Device1_GROGGIO09_AnIn1_mag_i,
+		IEDMODEL_Device1_GROGGIO10_AnIn1_mag_i};
 
-int main() {
-	simulate();
+DataAttribute *groupRealPowerTsLowDaNames[10] = {IEDMODEL_Device1_GROGGIO01_AnIn2_mag_i, IEDMODEL_Device1_GROGGIO02_AnIn2_mag_i, IEDMODEL_Device1_GROGGIO03_AnIn2_mag_i,
+		IEDMODEL_Device1_GROGGIO04_AnIn2_mag_i, IEDMODEL_Device1_GROGGIO05_AnIn2_mag_i, IEDMODEL_Device1_GROGGIO06_AnIn2_mag_i,
+		IEDMODEL_Device1_GROGGIO07_AnIn2_mag_i, IEDMODEL_Device1_GROGGIO08_AnIn2_mag_i, IEDMODEL_Device1_GROGGIO09_AnIn2_mag_i,
+		IEDMODEL_Device1_GROGGIO10_AnIn2_mag_i};
+
+DataAttribute *groupRealPowerDaNames[10] = {IEDMODEL_Device1_GROMMXU01_TotW_mag_i, IEDMODEL_Device1_GROMMXU02_TotW_mag_i, IEDMODEL_Device1_GROMMXU03_TotW_mag_i,
+		IEDMODEL_Device1_GROMMXU04_TotW_mag_i, IEDMODEL_Device1_GROMMXU05_TotW_mag_i, IEDMODEL_Device1_GROMMXU06_TotW_mag_i,
+		IEDMODEL_Device1_GROMMXU07_TotW_mag_i, IEDMODEL_Device1_GROMMXU08_TotW_mag_i, IEDMODEL_Device1_GROMMXU09_TotW_mag_i,
+		IEDMODEL_Device1_GROMMXU10_TotW_mag_i};
+
+// function prototypes
+void simulate(GroupInput *groupInput);
+void printOut(GroupInput *groupInput);
+void updateIedModel(IedServer iedServer, GroupInput *groupInput);
+
+/**
+ * Main function.
+ */
+int main(int argc, char** argv) {
+	printf("Using libIEC61850 version %s\n", LibIEC61850_getVersionString());
+
+	/* Create new server configuration object */
+	IedServerConfig config = IedServerConfig_create();
+
+	/* Set buffer size for buffered report control blocks to 200000 bytes */
+	IedServerConfig_setReportBufferSize(config, 200000);
+
+	/* Set stack compliance to a specific edition of the standard (WARNING: data model has also to be checked for compliance) */
+	IedServerConfig_setEdition(config, IEC_61850_EDITION_2);
+
+	/* Set the base path for the MMS file services */
+	IedServerConfig_setFileServiceBasePath(config, "./vmd-filestore/");
+
+	/* disable MMS file service */
+	IedServerConfig_enableFileService(config, false);
+
+	/* enable dynamic data set service */
+	IedServerConfig_enableDynamicDataSetService(config, true);
+
+	/* disable log service */
+	IedServerConfig_enableLogService(config, false);
+
+	/* set maximum number of clients */
+	IedServerConfig_setMaxMmsConnections(config, 2);
+
+	/* Create a new IEC 61850 server instance */
+	iedServer = IedServer_createWithConfig(&iedModel, NULL, config);
+
+	/* configuration object is no longer required */
+	IedServerConfig_destroy(config);
+
+	/* set the identity values for MMS identify service */
+	IedServer_setServerIdentity(iedServer, "Hwacom", "mms servcer", "1.5");
+
+	IedServer_setConnectionIndicationHandler(iedServer, (IedConnectionIndicationHandler) connectionHandler, NULL);
+
+	IedServer_start(iedServer, MMS_PORT);
+
+	if (!IedServer_isRunning(iedServer)) {
+		printf("Starting server failed (maybe need root permissions or another server is already using the port)! Exit.\n");
+		IedServer_destroy(iedServer);
+		exit(-1);
+	}
+
+	running = 1;
+
+	signal(SIGINT, sigint_handler);
+
+	while (running) {
+		uint64_t timestamp = Hal_getTimeInMs();
+		Timestamp iecTimestamp;
+		Timestamp_clearFlags(&iecTimestamp);
+		Timestamp_setTimeInMilliseconds(&iecTimestamp, timestamp);
+		Timestamp_setLeapSecondKnown(&iecTimestamp, true);
+
+		GroupInput *groupInput = malloc(sizeof(GroupInput));
+		simulate(groupInput);
+		updateIedModel(iedServer, groupInput);
+		printOut(groupInput);
+		free(groupInput);
+		Thread_sleep(1000);
+	}
+
+	/* stop MMS server - close TCP server socket and all client sockets */
+	IedServer_stop(iedServer);
+
+	/* Cleanup - free all resources */
+	IedServer_destroy(iedServer);
+
+	return 0;
 }
 
-void simulate() {
-	GroupInput *groupInput = malloc(sizeof(GroupInput));
+void simulate(GroupInput *groupInput) {
 	groupInput->serviceStart = true;
 	groupInput->serviceStop = false;
-	groupInput->providerId = 8477;
-	groupInput->quotationId = 8623;
+	groupInput->providerId = 84778623;
+	groupInput->quotationId = 6163;
 	groupInput->serviceCode = 0;
 	groupInput->supplyWatt = 12000;
 	groupInput->demandWatt = 10000;
-	groupInput->timestamp = (unsigned long)time(NULL);
+	groupInput->timestamp = Hal_getTimeInMs();
 	groupInput->executionRate = 8888;
 	for(int i=0; i<10; i++) {
 		GroupRealPower *groupRealPower = malloc(sizeof(GroupRealPower));
 		groupRealPower->totalWatt = 10000 - 1000 * sin(2 * PI * (40 * i) / 360);
-		groupRealPower->timestamp = groupInput->timestamp - (10 - i);
+		groupRealPower->timestamp = groupInput->timestamp - (10 - i) * 1000;
 		groupInput->realPower[i] = *groupRealPower;
 	}
+}
 
+void printOut(GroupInput *groupInput) {
 	printf("%21s: %d\n", "service start", groupInput->serviceStart);
 	printf("%21s: %d\n", "service stop", groupInput->serviceStop);
 	printf("%21s: %05d\n", "provider id", groupInput->providerId);
@@ -83,5 +195,41 @@ void simulate() {
 	}
 	printf("%21s: %5d\n", "execution rate", groupInput->executionRate);
 	printf("%21s: %ld\n", "timestamp", groupInput->timestamp);
-	free(groupInput);
+}
+
+void updateIedModel(IedServer iedServer, GroupInput *groupInput) {
+	IedServer_lockDataModel(iedServer);
+
+	Timestamp iecTimestamp;
+	Timestamp_clearFlags(&iecTimestamp);
+	Timestamp_setTimeInMilliseconds(&iecTimestamp, groupInput->timestamp);
+	Timestamp_setLeapSecondKnown(&iecTimestamp, true);
+
+	IedServer_updateBooleanAttributeValue(iedServer, IEDMODEL_Device1_DREGGIO01_Ind1_stVal, groupInput->serviceStart);
+	IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_Device1_DREGGIO01_Ind1_t,  &iecTimestamp);
+	IedServer_updateBooleanAttributeValue(iedServer, IEDMODEL_Device1_DREGGIO02_Ind1_stVal, groupInput->serviceStop);
+	IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_Device1_DREGGIO02_Ind1_t, &iecTimestamp);
+	IedServer_updateInt32AttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_IntIn1_stVal, groupInput->providerId);
+	IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_IntIn1_t, &iecTimestamp);
+	IedServer_updateInt32AttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_IntIn2_stVal, groupInput->quotationId);
+	IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_IntIn2_t, &iecTimestamp);
+	IedServer_updateInt32AttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_IntIn3_stVal, groupInput->serviceCode);
+	IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_IntIn3_t, &iecTimestamp);
+	IedServer_updateInt32AttributeValue(iedServer, IEDMODEL_Device1_GROMMXU01_TotW_mag_i, groupInput->supplyWatt);
+	IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_Device1_GROMMXU01_TotW_t, &iecTimestamp);
+	// see 'How to store a 64 bit integer in two 32 bit integers and convert back again' in stackoverflow
+	IedServer_updateInt32AttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_AnIn1_mag_i, (uint32_t)(groupInput->timestamp >> 32));
+	IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_AnIn1_t, &iecTimestamp);
+	IedServer_updateInt32AttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_AnIn2_mag_i, (uint32_t)(groupInput->timestamp));
+	IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_AnIn2_t, &iecTimestamp);
+	IedServer_updateInt32AttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_AnIn3_mag_i, groupInput->executionRate);
+	IedServer_updateTimestampAttributeValue(iedServer, IEDMODEL_Device1_GROGGIO_AnIn3_t, &iecTimestamp);
+    char *daName = "IEDMODEL_Device1_GROGGIO01_AnIn1_mag_i";
+	for(int i=0; i<10; i++) {
+		IedServer_updateInt32AttributeValue(iedServer, groupRealPowerTsHighDaNames[i], (uint32_t)(groupInput->realPower[i].timestamp >> 32));
+		IedServer_updateInt32AttributeValue(iedServer, groupRealPowerTsLowDaNames[i], (uint32_t)(groupInput->realPower[i].timestamp));
+		IedServer_updateInt32AttributeValue(iedServer, groupRealPowerDaNames[i], groupInput->realPower[i].totalWatt);
+	}
+
+	IedServer_unlockDataModel(iedServer);
 }
--- iec-61850/note.md
+++ iec-61850/note.md
@@ -4,3 +4,8 @@ status information，前者出現在輔助服務傳輸格式說明中的AI，後
 - 在7-1裡用GGIO做搜尋，找到幾個例子，看來DO名就直接寫Ind1
 - LD/GROGGIO01.AnIn1.mag.i、LD/GROGGIO01.AnIn2.mag.i標註為Unix Timestamp-H與Unix Timestamp-L，
 參考[How Do I Calculate the Timestamp in UTC in an Enhanced Packet Block?](https://ask.wireshark.org/question/15177/how-do-i-calculate-the-timestamp-in-utc-in-an-enhanced-packet-block/)，所謂的High和Low是64 bits timestmp的前32與後32
+- `sudo make INSTALL_PREFIX=/usr/local install`解決iec61850_server.h等unresolved inclusion的問題
+> mkdir -p /usr/local/include
+mkdir -p /usr/local/lib
+cp hal/inc/hal_time.h  hal/inc/hal_thread.h hal/inc/hal_filesystem.h hal/inc/tls_config.h hal/inc/lib_memory.h src/common/inc/libiec61850_common_api.h src/common/inc/linked_list.h src/iec61850/inc/iec61850_client.h src/iec61850/inc/iec61850_common.h src/iec61850/inc/iec61850_server.h src/iec61850/inc/iec61850_model.h src/iec61850/inc/iec61850_cdc.h src/iec61850/inc/iec61850_dynamic_model.h src/iec61850/inc/iec61850_config_file_parser.h src/mms/inc/mms_value.h src/mms/inc/mms_common.h src/mms/inc/mms_types.h src/mms/inc/mms_type_spec.h src/mms/inc/mms_client_connection.h src/mms/inc/iso_connection_parameters.h src/goose/goose_subscriber.h src/goose/goose_receiver.h src/goose/goose_publisher.h src/sampled_values/sv_subscriber.h src/sampled_values/sv_publisher.h src/logging/logging_api.h /usr/local/include
+cp ./build/libiec61850.a /usr/local/lib
